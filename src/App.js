414e360e5cf23a6fdd020b60579a7f3db9d9e6de
abhishek12201 <95902938+abhishek12201@users.noreply.github.com>
2024-05-21 14:23:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SpeedTypingGame from './components/SpeedTypingGame';
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Typing Test </h1>
+      <SpeedTypingGame />
+    </div>
+  );
+}
+
+export default App;
